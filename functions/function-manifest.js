86c91b48e60cbba12c8681bfa2c66a5608e7468a
Brian Zalewski <59970525+ProfessorManhattan@users.noreply.github.com>
2025-04-25 20:13:21 +0000

    Poke

--- functions/function-manifest.js
+++ functions/function-manifest.js
@@ -57,6 +57,33 @@ const tools = [
       },
     },
   },
+  {
+    type: 'function',
+    function: {
+      name: 'transferCall',
+      say: 'One moment while I transfer your call.',
+      description: 'Transfers the customer to a live agent in case they request help from a real person.',
+      parameters: {
+        type: 'object',
+        properties: {
+          callSid: {
+            type: 'string',
+            description: 'The unique identifier for the active phone call.',
+          },
+        },
+        required: ['callSid'],
+      },
+      returns: {
+        type: 'object',
+        properties: {
+          status: {
+            type: 'string',
+            description: 'Whether or not the customer call was successfully transfered'
+          },
+        }
+      }
+    },
+  },
 ];
 
 module.exports = tools;
